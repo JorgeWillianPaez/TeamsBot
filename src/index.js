09cb24a46739baece21c3d2e947541241b64c39b
Jorge Willian Paez Nagakura <jorgewillianpaez@gmail.com>
2025-06-02 20:19:06 -0300

    Atualizando

--- src/index.js
+++ src/index.js
@@ -48,29 +48,61 @@ app.post("/webhook", async (req, res) => {
     const phoneNumberId = value.metadata.phone_number_id;
     const from = tratarNumeroBrasileiro(value.messages[0].from);
     const msgBody = value.messages[0].text.body;
+    console.log("Value:", value);
+    // console.log("ID do telefone:", phoneNumberId);
+    // console.log("Número do remetente:", from);
+    // console.log("Mensagem recebida:", msgBody);
 
     const cliente = new OpenAI({
-        apiKey: process.env.OPENAI_API_KEY,
+      apiKey: process.env.OPENAI_API_KEY,
     })
-    
+
     const modelo = "gpt-4o-mini";
 
     const url = "https://zenthos.com.br/system/api/QCLmi1UVW45wVzPWbgxKTtRgEkiJkZ51BrAA6xSC8PPtr4UECHXOZmoIcZDFZyVk93";
 
-    console.log("Mensagem recebida:", msgBody);
-
     try {
-
-      // const response = await fetch(url);
       // console.log("Response:", response);
-      // const data = await response.json();
+      const data = await response.json();
       // console.log("Data:", data);
 
-      //   const dados = data.map(item => 
-      //     `Pergunta: ${item.pergunta} Resposta: ${item.resposta}`
-      //   ).join("\n");
+      const dados = data.map(item => 
+        `Pergunta: ${item.pergunta} Resposta: ${item.resposta}`
+      ).join("\n");
 
-      //   const prompt_sistema = `
+      const prompt_sistema = `
+      Seu nome é Claudete. Você é uma especialista do setor de Recursos Humanos de uma empresa que ajuda a esclarecer as dúvidas das pessoas sobre férias, folha de pagamento, benefícios e outras questões relacionadas a área de Recursos Humanos.
+      Você pode utilizar como base para elaborar a melhor resposta a lista abaixo.
+      Escreva um parágrafo com menos de 50 palavras resumindo a resposta mais adequada para a pergunta: ${msgBody}.
+
+      # Lista de Respostas
+      ${dados}
+
+      # Formato da Saída
+      [Utilize uma linguagem simples para as respostas]
+      `;
+
+      // const dados = `
+      //   - A empresa não permite vender férias.
+      //   - É permitido separar as férias das seguintes maneiras: 3 vezes de 10 dias, 2 vezes de 15 dias ou 30 dias.
+      //   - Precisa solicitar as férias no portal do RH.
+      //   - As férias devem ser solicitadas com 30 dias de antecedência.
+      //   - O pagamento referente as férias ocorre em até 2 dias úteis antes do início de descanso.
+      //   - Em caso de falta no trabalho, o colaborador precisa apresentar um atestado médico, caso contrário será descontado do pagamento o período que esteve ausente.
+      //   - O empregado tem direito, anualmente, ao gozo de um período de férias de 30 dias.
+      //   - O período será sempre de 30 dias? Não, isso depende de quantas faltas injustificadas o empregado teve durante o período aquisitivo de férias. Veja a tabela de dias de férias de acordo com as faltas: 
+      //   - 30 (trinta) dias corridos, quando não houver faltado ao serviço mais de 5 (cinco) vezes.
+      //   - 24 (vinte e quatro) dias corridos, quando houver tido de 6 (seis) a 14 (quatorze) faltas.
+      //   - 18 (dezoito) dias corridos, quando houver tido de 15 (quinze) a 23 (vinte e três) faltas.
+      //   - 12 (doze) dias corridos, quando houver tido de 24 (vinte e quatro) a 32 (trinta e duas) faltas.
+      //   As faltas justificadas por atestado médico ou qualquer das hipóteses previstas em lei, não são levadas em consideração para fins de contagem do período de descanso.
+      //   - Quando serão concedidas as férias ao empregado? Para que o empregado tenha direito a tirar férias, é preciso cumprir um período chamado de período aquisitivo que corresponde a 12 meses de trabalho.
+      //   - Após trabalhar 12 meses, o trabalhador cumpriu o período aquisitivo e entra automaticamente no período concessivo de 12 meses que o empregador tem para conceder as férias. Resumindo: Depois que o empregado completar 12 meses de trabalho, o empregador terá os 12 meses seguintes para conceder o descanso do trabalhador.
+      //   - O período de descanso remunerado deve ser anotado na carteira de trabalho do empregado? Sim. Inclusive, o empregado só poderá entrar de férias depois que a anotação tiver devidamente concluída.
+      //   - Parentes que trabalham na mesma empresa podem tirar férias juntos? Sim. A lei assegura que, se assim quiserem, membros da mesma família tirem férias juntos, desde que isso não traga prejuízos ao serviço.
+      //   `;
+
+      // const prompt_sistema = `
       //   Seu nome é Claudete. Você é uma especialista do setor de Recursos Humanos de uma empresa que ajuda a esclarecer as dúvidas das pessoas sobre férias, folha de pagamento, benefícios e outras questões relacionadas a área de Recursos Humanos.
       //   Você pode utilizar como base para elaborar a melhor resposta a lista abaixo.
       //   Escreva um parágrafo com menos de 50 palavras resumindo a resposta mais adequada para a pergunta: ${msgBody}.
@@ -82,40 +114,6 @@ app.post("/webhook", async (req, res) => {
       //   [Utilize uma linguagem simples para as respostas]
       // `;
 
-      const dados = `
-        - A empresa não permite vender férias.
-        - É permitido separar as férias das seguintes maneiras: 3 vezes de 10 dias, 2 vezes de 15 dias ou 30 dias.
-        - Precisa solicitar as férias no portal do RH.
-        - As férias devem ser solicitadas com 30 dias de antecedência.
-        - O pagamento referente as férias ocorre em até 2 dias úteis antes do início de descanso.
-        - Em caso de falta no trabalho, o colaborador precisa apresentar um atestado médico, caso contrário será descontado do pagamento o período que esteve ausente.
-        - O empregado tem direito, anualmente, ao gozo de um período de férias de 30 dias.
-        - O período será sempre de 30 dias? Não, isso depende de quantas faltas injustificadas o empregado teve durante o período aquisitivo de férias. Veja a tabela de dias de férias de acordo com as faltas: 
-        - 30 (trinta) dias corridos, quando não houver faltado ao serviço mais de 5 (cinco) vezes.
-        - 24 (vinte e quatro) dias corridos, quando houver tido de 6 (seis) a 14 (quatorze) faltas.
-        - 18 (dezoito) dias corridos, quando houver tido de 15 (quinze) a 23 (vinte e três) faltas.
-        - 12 (doze) dias corridos, quando houver tido de 24 (vinte e quatro) a 32 (trinta e duas) faltas.
-        As faltas justificadas por atestado médico ou qualquer das hipóteses previstas em lei, não são levadas em consideração para fins de contagem do período de descanso.
-        - Quando serão concedidas as férias ao empregado? Para que o empregado tenha direito a tirar férias, é preciso cumprir um período chamado de período aquisitivo que corresponde a 12 meses de trabalho.
-        - Após trabalhar 12 meses, o trabalhador cumpriu o período aquisitivo e entra automaticamente no período concessivo de 12 meses que o empregador tem para conceder as férias. Resumindo: Depois que o empregado completar 12 meses de trabalho, o empregador terá os 12 meses seguintes para conceder o descanso do trabalhador.
-        - O período de descanso remunerado deve ser anotado na carteira de trabalho do empregado? Sim. Inclusive, o empregado só poderá entrar de férias depois que a anotação tiver devidamente concluída.
-        - Parentes que trabalham na mesma empresa podem tirar férias juntos? Sim. A lei assegura que, se assim quiserem, membros da mesma família tirem férias juntos, desde que isso não traga prejuízos ao serviço.
-        `;
-
-        const prompt_sistema = `
-        Seu nome é Claudete. Você é uma especialista do setor de Recursos Humanos de uma empresa que ajuda a esclarecer as dúvidas das pessoas sobre férias, folha de pagamento, benefícios e outras questões relacionadas a área de Recursos Humanos.
-        Você pode utilizar como base para elaborar a melhor resposta a lista abaixo.
-        Escreva um parágrafo com menos de 50 palavras resumindo a resposta mais adequada para a pergunta: ${msgBody}.
-        
-        # Lista de Respostas
-        ${dados}
-        
-        # Formato da Saída
-        [Utilize uma linguagem simples para as respostas]
-      `;
-
-      console.log("Prompt:", prompt_sistema);
-
       const resposta = await cliente.chat.completions.create({
         model: modelo,
         messages: [
@@ -126,8 +124,9 @@ app.post("/webhook", async (req, res) => {
         max_tokens: 100,
       });
 
-      console.log("Resposta:", resposta.choices[0].message.content);
-
+      console.log("Phone Number ID:", phoneNumberId);
+      console.log("Token:", token);
+      
       await axios({
         method: "POST",
         url: `https://graph.facebook.com/v22.0/${phoneNumberId}/messages?access_token=${token}`,
